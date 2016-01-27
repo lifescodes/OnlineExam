497cf852f8bf588f3d6d5bd53b697683b82c63ae
dayatz <dayat.py@gmail.com>
2016-01-27 13:15:02 +0800

    add bootstrap files, basic base template

--- backend/online_exam/config/settings/base.py
+++ backend/online_exam/config/settings/base.py
@@ -110,7 +110,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/s/'
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'))
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 MEDIA_URL = '/m/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
